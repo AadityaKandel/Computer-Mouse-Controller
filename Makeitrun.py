949865bc01dc9d593efef1b4df550d5e7ede6ade
AK HACKS OR TRICKS <71229713+AadityaKandel@users.noreply.github.com>
2020-11-24 19:26:28 +0545

    Create Makeitrun.py
    If That Python Program Doesn't Work
    Make Sure Python Is Installed And Pip Is Also Installed
    
    Run Makeitrun.py

---
+++ Makeitrun.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+# Importing Os Success
+
+print('Just Wait')
+time.sleep(2)
+
+os.system('pip install keyboard')
+os.system('pip install pyautogui')
+
+os.system('cls')
+print('Now Run That File, Okay ?')
+print(''':) :) :) :) :) :) :) :)''')
+print('Press ANy Key..')
+a = input()
